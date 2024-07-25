aad5eb4279dbd0719082484804fd5041f8743795
aagam1994 <aagam1994@gmail.com>
2024-07-24 21:52:53 -0400

    adding Bash Scripts

---
+++ Week 12/README.md
@@ -0,0 +1,3 @@
+# NTWK8141 - Week 12
+
+This folder includes all Bash Scripts written and tested for Week 12 of this course.
---
+++ Week 12/myScript.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# A simple script that uses variables - Aagam Shah
+
+echo "I am logged into the $USER account."
+echo "The filename for this script is $0."
+echo "The input for this file is $1."
+
+exit
